7dd3d1417776b72672fb05eb21fb3e8fd2232dab
Sergey Avdeenkov <32090146+itamitut@users.noreply.github.com>
2020-03-31 16:10:32 +0300

    Added logging

--- pages/all_books_page.py
+++ pages/all_books_page.py
@@ -1,10 +1,21 @@
 from bs4 import BeautifulSoup
 from locators.all_book_page import AllBooksPageLocators
 from parsers.book_parser import BookParser
+import re
+
 
 class AllBooksPage:
     def __init__(self, page_content):
         self.soup = BeautifulSoup(page_content, 'html.parser')
+
     @property
     def books(self):
-        return [BookParser(e) for e in self.soup.select(AllBooksPageLocators.BOOKS)]
+        return [BookParser(e) for e in self.soup.select(AllBooksPageLocators.BOOKS)]\
+
+    @property
+    def page_count(self):
+        content = self.soup.select_one(AllBooksPageLocators.PAGER).string
+        pattern = 'Page [0-9]+ of ([0-9]+)'
+        matcher = re.search(pattern, content)
+        page_count = int(matcher.group(1))
+        return page_count
